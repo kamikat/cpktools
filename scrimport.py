ba06927be3aa6aeda6e16f8ab6e10408c4797abd
trinity <imandry.c@gmail.com>
2013-05-12 18:43:52 +0800

    scrimport as text import(replace) tool

--- scrimport.py
+++ scrimport.py
@@ -1,41 +1,54 @@
 #!/usr/bin/env python
 
+import argparse
+
+parser = argparse.ArgumentParser(description='Plant text back to original file')
+parser.add_argument('source', help='original scenario file')
+parser.add_argument('inputs', nargs='+', help='text file(s) with tag')
+parser.add_argument('-o', '--output', default='out.bin',
+        help='output destination') 
+args = parser.parse_args()
+
 import struct
 import re
 
-src = open('scr.bin', 'rb').read()
+src = open(args.source, 'rb').read()
 
 p = re.compile(r"(0x[0-9A-Fa-f]{10})-(0x[0-9A-Fa-f]{10})\((0x[0-9A-Fa-f]{4})\)\t(.*)\r$")
 
 def unpack_line(line):
     return tuple(filter(None, p.split(line)))
 
-txt = map(
-        lambda (x): x.lstrip(), 
-        open('scr.utf8.txt', 'rb').read().split('\n')
+txt = sorted(
+        filter(lambda (x): not x.startswith('#') and not len(x) == 0,
+            map(lambda (x): x.lstrip(), 
+                reduce(lambda (a, b): a + b, 
+                    map(lambda (f): open(f, 'rb').read().split('\n'), 
+                        args.inputs
+                        )
+                    )
+                )
+            )
         )
 
-output = open('scr.packed.bin', 'wb')
+with open(args.output, 'wb') as output:
 
-srcptr = 0
+    srcptr = 0
 
-for i in xrange(len(txt) - 1):
+    for line in txt:
 
-    if txt[i].startswith("#"):
-        continue
-    
-    (start, end, length, text) = unpack_line(txt[i])
+        (start, end, length, text) = unpack_line(line)
 
-    output.write(src[srcptr:int(start, 0)])
+        output.write(src[srcptr:int(start, 0)])
 
-    data = text.decode('utf-8').encode('shift-jis')
+        data = text.decode('utf-8').encode('shift-jis')
 
-    output.write(struct.pack("<h", len(data) + 4))
-    output.write(data)
-    output.write('\x00\x00')
+        output.write(struct.pack("<h", len(data) + 4))
+        output.write(data)
+        output.write('\x00\x00')
 
-    srcptr = int(end, 0)
+        srcptr = int(end, 0)
 
-output.write(src[srcptr:])
+    output.write(src[srcptr:])
 
-output.close()
+exit(0)
