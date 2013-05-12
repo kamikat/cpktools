a011791cce287914bab2e7b5b70447cafdeea55b
trinity <imandry.c@gmail.com>
2013-05-12 12:24:38 +0800

    edit debug utility

--- process.sh
+++ process.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-cat $1 | python -c 'import sys; sys.stdout.write(sys.stdin.read()[::-1])' | xxd -u > $2
+cat $1 | python -c 'import sys; sys.stdout.write(sys.stdin.read()[::-1])' > $2
 
-# Reverse Bytes in $1, convert to hexdump and write to $2
+cat $2 | xxd -u > $3
+
+# Reverse Bytes in $1 write to $2, convert to hexdump and write to $3
