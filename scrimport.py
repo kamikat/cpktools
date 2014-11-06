f81a666082679f5cc563b06c9f7b9cedf0c74855
kirisetsz <kirisetsz@gmail.com>
2014-11-06 22:34:59 +0800

    update scrimport which reads an external codemap

--- scrimport.py
+++ scrimport.py
@@ -4,9 +4,10 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Plant text back to original file')
 parser.add_argument('source', help='original scenario file')
-parser.add_argument('inputs', nargs='+', help='text file(s) with tag')
+parser.add_argument('text', help='text file with tags')
 parser.add_argument('-o', '--output', default='out.bin',
-        help='output destination') 
+        help='output destination')
+parser.add_argument('-c', '--codefile', help='code file')
 args = parser.parse_args()
 
 import struct
@@ -21,15 +22,23 @@ def unpack_line(line):
 
 txt = sorted(
         filter(lambda (x): not x.startswith('#') and not len(x) == 0,
-            map(lambda (x): x.lstrip(), 
-                reduce(lambda (a, b): a + b, 
-                    map(lambda (f): open(f, 'rb').read().split('\n'), 
-                        args.inputs
-                        )
-                    )
+            map(lambda (x): x.lstrip(),
+                open(args.text, 'rb').read().split('\n')
                 )
             )
         )
+
+codetable = {}
+
+if args.codefile:
+    with open(args.codefile, 'r') as codefile:
+        while True:
+            line = codefile.readline().strip('\r\n')
+            if len(line):
+                code, character = line.decode('utf-8').split('=', 1)
+                codetable[character] = struct.pack('>H', int(code, 16))
+            else:
+                break
 
 with open(args.output, 'wb') as output:
 
@@ -42,10 +51,20 @@ with open(args.output, 'wb') as output:
         except:
             continue
 
+        text = text.decode('utf-8')
+
+        if args.codefile:
+            data = ''
+            for character in text:
+                if codetable.has_key(character):
+                    data += codetable[character]
+                else:
+                    print '[WARNING] Missing character', character.encode('utf-8'), 'at', line
+                    continue
+        else:
+            data = text.encode('shift-jis')
+
         output.write(src[srcptr:int(start, 0)])
-
-        data = text.decode('utf-8').encode('shift-jis')
-
         output.write(struct.pack("<h", len(data) + 4))
         output.write(data)
         output.write('\x00\x00')
